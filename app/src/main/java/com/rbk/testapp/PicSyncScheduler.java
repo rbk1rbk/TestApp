5304c8e202a11c92891b8b7322d8af4cd7cf0177
biel <biel.urban@gmail.com>
2016-09-20 23:53:37 +0200

    bugfixing

--- app/src/main/java/com/rbk/testapp/PicSyncScheduler.java
+++ app/src/main/java/com/rbk/testapp/PicSyncScheduler.java
@@ -13,19 +13,23 @@ import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
-import android.support.v4.content.LocalBroadcastManager;
+import android.text.TextUtils;
 import android.util.Log;
 
 public class PicSyncScheduler extends Service {
 	static private boolean onUnmeteredNetwork=false;
 	static private boolean onHomeWifi=false;
 	static private boolean isconnected=false;
+	static private boolean isChargingCondition=false;
+	static private boolean isCharging=false;
+	static private boolean chargerEventReceiverRegistered=false;
 	static private boolean changeInMediaFolders=false;
 	static private int nextWakeUpTime=0;
 	int callBack=0;
 
 	private final Context myContext=this;
 
+	private static SharedPreferences settings;
 
 	/*
 	* This class provides a Watchdog type of service, that calls back
@@ -38,6 +42,37 @@ public class PicSyncScheduler extends Service {
 	* */
 	private static boolean wifiChangeReceiverRegistered = false;
 	private static ConnectivityManager cm;
+
+	SharedPreferences.OnSharedPreferenceChangeListener prefChangeListener =
+			new SharedPreferences.OnSharedPreferenceChangeListener() {
+				public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+					if (TextUtils.equals(key,"pref_switch_charging_condition")) {
+						handleChargingConditions();
+					}
+					if (TextUtils.equals(key,"pref_homewifissid")) {
+						wifiChangeReceiver.onReceive(myContext,new Intent("android.net.conn.CONNECTIVITY_CHANGE"));
+					}
+				}
+			};
+
+	private BroadcastReceiver chargerPluggedReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			isCharging=true;
+			Log.d("PicSyncScheduler","Charger plugged in.");
+			evaluateTheNeedOfSync();
+		}
+	};
+
+	private BroadcastReceiver chargerUnpluggedReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			isCharging=false;
+			Log.d("PicSyncScheduler","Charger unplugged.");
+			evaluateTheNeedOfSync();
+		}
+	};
+
 	private BroadcastReceiver wifiChangeReceiver = new BroadcastReceiver() {
 		int connType;
 		WifiManager wifiManager=null;
@@ -73,9 +108,11 @@ public class PicSyncScheduler extends Service {
 						onHomeWifi=true;
 						Log.d("PicSyncScheduler","Home WiFi detected");
 					}
-					else
-						onHomeWifi=false;
-						Log.d("PicSyncScheduler","Other than home WiFi detected");
+					else {
+						onHomeWifi = false;
+						Log.d("PicSyncScheduler", "Other than home WiFi detected");
+					}
+					evaluateTheNeedOfSync();
 				}
 			}
 /*
@@ -90,6 +127,12 @@ public class PicSyncScheduler extends Service {
 		}
 	};
 
+	private void evaluateTheNeedOfSync() {
+		if (onHomeWifi && isCharging)
+			Log.d("PicSyncScheduler","We should start sync");
+		else
+			Log.d("PicSyncScheduler","We should NOT start sync. If it is running, stop it.");
+	}
 
 
 	public PicSyncScheduler() {
@@ -103,18 +146,58 @@ public class PicSyncScheduler extends Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		settings = PreferenceManager.getDefaultSharedPreferences(this);
+		settings.registerOnSharedPreferenceChangeListener(prefChangeListener);
 		if (!wifiChangeReceiverRegistered){
 			registerReceiver(wifiChangeReceiver, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
 			wifiChangeReceiverRegistered=true;
 			Log.d("PicSyncScheduler","wifiChangeReceiver registered");
 		}
+		handleChargingConditions();
 	}
 
+	private boolean isChargingConditionRequired() {
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(myContext);
+		return settings.getBoolean("pref_switch_charging_condition", false);
+	}
+	private void registerChargerEventReceiver(){
+		if (!chargerEventReceiverRegistered) {
+			registerReceiver(chargerPluggedReceiver, new IntentFilter("android.intent.action.ACTION_POWER_CONNECTED"));
+			registerReceiver(chargerUnpluggedReceiver, new IntentFilter("android.intent.action.ACTION_POWER_DISCONNECTED"));
+			chargerEventReceiverRegistered = true;
+		}
+	}
+	private void unregisterChargerEventReceiver(){
+		if (chargerEventReceiverRegistered) {
+			unregisterReceiver(chargerPluggedReceiver);
+			unregisterReceiver(chargerUnpluggedReceiver);
+			chargerEventReceiverRegistered = false;
+		}
+
+	}
+	private void handleChargingConditions(){
+		isChargingCondition = isChargingConditionRequired();
+		if (isChargingCondition && !chargerEventReceiverRegistered){
+			Log.d("PicSyncScheduler","Charging events listeners registered");
+			registerChargerEventReceiver();
+		}
+		else{
+			unregisterChargerEventReceiver();
+			isCharging=true;
+		}
+	}
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		unregisterReceiver(wifiChangeReceiver);
-		wifiChangeReceiverRegistered=false;
+		if (wifiChangeReceiverRegistered) {
+			unregisterReceiver(wifiChangeReceiver);
+			wifiChangeReceiverRegistered = false;
+		}
+		if (chargerEventReceiverRegistered){
+			unregisterChargerEventReceiver();
+		}
+		settings.unregisterOnSharedPreferenceChangeListener(prefChangeListener);
+
 	}
 
 	@Override
